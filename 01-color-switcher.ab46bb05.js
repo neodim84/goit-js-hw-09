f08adf1abe163ab912ce3be284d3e2c847f8a8a3
neodim84 <neodim84@gmail.com>
2022-10-06 06:13:22 +0000

    Deploying to gh-pages from @ neodim84/goit-js-hw-09@000e091b597db323e906ea6566f139923906ce0d 🚀

---
+++ 01-color-switcher.ab46bb05.js
@@ -0,0 +1,2 @@
+!function(){var t=document.body,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(n){a=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(a),e.disabled=!1}));var a=null}();
+//# sourceMappingURL=01-color-switcher.ab46bb05.js.map
---
+++ 01-color-switcher.b772d424.js
@@ -0,0 +1,2 @@
+const t=document.body,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(n){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}));let o=null;
+//# sourceMappingURL=01-color-switcher.b772d424.js.map
